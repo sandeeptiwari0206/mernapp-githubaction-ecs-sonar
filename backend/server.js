c9ae13e88ac8e0d1f30e1f0fc66282b3a1a0960f
sandeeptiwari0206 <sandeeptiwari.infi@gmail.com>
2026-01-29 10:53:20 +0000

    updated code

--- backend/server.js
+++ backend/server.js
@@ -1,45 +1,46 @@
-server. js
-
-
-
 import path from 'path';
 import express from 'express';
 import dotenv from 'dotenv';
-dotenv.config();
-import connectDB from './config/db.js';
 import cookieParser from 'cookie-parser';
-import { notFound, errorHandler } from './middleware/errorMiddleware.js';
+import connectDB from './config/db.js';
 import userRoutes from './routes/userRoutes.js';
+import { notFound, errorHandler } from './middleware/errorMiddleware.js';
 
-const port = process.env.PORT || 5000;
+dotenv.config();
 
+// Connect to MongoDB
 connectDB();
 
 const app = express();
 
+// Middleware to parse JSON and URL-encoded data
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
-
 app.use(cookieParser());
 
+// Routes
 app.use('/api/users', userRoutes);
 
+// Serve frontend in production
 if (process.env.NODE_ENV === 'production') {
   const __dirname = path.resolve();
-  app.use(express.static(path.join(__dirname, '/frontend/dist')));
+  app.use(express.static(path.join(__dirname, 'frontend', 'dist')));
 
   app.get('*', (req, res) =>
-    res.sendFile(path.resolve(__dirname, 'frontend', 'dist', 'index.html'))
+    res.sendFile(path.join(__dirname, 'frontend', 'dist', 'index.html'))
   );
 } else {
   app.get('/', (req, res) => {
-    res.send('API is running....');
+    res.send('API is running...');
   });
 }
 
+// Error handling middleware
 app.use(notFound);
 app.use(errorHandler);
 
-app.listen(port, "0.0.0.0", () => {
-  console.log(`Server is started on port ${port}`);
+// Start the server
+const PORT = process.env.PORT || 5000;
+app.listen(PORT, '0.0.0.0', () => {
+  console.log(`Server is running on port ${PORT}`);
 });
