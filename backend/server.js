49103252aebe60f26dca58e457411078c2ffad40
sandeeptiwari0206 <sandeeptiwari.infi@gmail.com>
2026-01-29 10:22:30 +0000

    updated code

--- backend/server.js
+++ backend/server.js
@@ -1,15 +1,45 @@
-import dotenv from 'dotenv';
-import connectDB from './config/db.js';
-import app from './app.js';
+server. js
 
+
+
+import path from 'path';
+import express from 'express';
+import dotenv from 'dotenv';
 dotenv.config();
+import connectDB from './config/db.js';
+import cookieParser from 'cookie-parser';
+import { notFound, errorHandler } from './middleware/errorMiddleware.js';
+import userRoutes from './routes/userRoutes.js';
 
 const port = process.env.PORT || 5000;
 
-/* ---------- Database ---------- */
 connectDB();
 
-/* ---------- Server ---------- */
-app.listen(port, '0.0.0.0', () => {
+const app = express();
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+app.use(cookieParser());
+
+app.use('/api/users', userRoutes);
+
+if (process.env.NODE_ENV === 'production') {
+  const __dirname = path.resolve();
+  app.use(express.static(path.join(__dirname, '/frontend/dist')));
+
+  app.get('*', (req, res) =>
+    res.sendFile(path.resolve(__dirname, 'frontend', 'dist', 'index.html'))
+  );
+} else {
+  app.get('/', (req, res) => {
+    res.send('API is running....');
+  });
+}
+
+app.use(notFound);
+app.use(errorHandler);
+
+app.listen(port, "0.0.0.0", () => {
   console.log(`Server is started on port ${port}`);
 });
--- jest.config.mjs
+++
@@ -1,9 +0,0 @@
-export default {
-  testEnvironment: 'node',
-
-  testMatch: ['**/tests/**/*.test.js'],
-
-  collectCoverage: true,
-  coverageDirectory: 'coverage',
-  coverageReporters: ['lcov', 'text']
-};
